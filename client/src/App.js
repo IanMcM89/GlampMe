da6704327cb35c1d89ede72ff53e2c8c14f64f9f
Ian <ianmcm.89@gmail.com>
2023-02-18 15:29:44 -0500

    set up initial routing structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import styled from "styled-components";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+      <Switch>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/services">
+          <Services />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Main>
   );
 }
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 90vh;
+`;
 
 export default App;
