2eba32ebf1508977d850fe9dd9f762dbdf3af6ae
Ian <ianmcm.89@gmail.com>
2023-02-18 18:12:42 -0500

    tab styling and structure changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,63 @@
 import React from "react";
-import { Routes ,Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header";
+import Footer from "./components/footer";
 import styled from "styled-components";
 import "./App.css";
 
 function App() {
   return (
-    <Main>
-      <Routes>
-        <Route path="/services" />
-        <Route path="/contact" />
-        <Route path="/about" />
-        <Route path="/" element={<div/>}/>
-      </Routes>
-    </Main>
+    <>
+      <Header />
+      <Main>
+        {/* <Routes>
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Routes> */}
+        <Home src="/images/bonfire.jpg" alt="" />
+        <Services />
+        <About />
+        <Contact />
+        <Footer />
+      </Main>
+    </>
   );
 }
 
-export const Main = styled.main`
+const Main = styled.main`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   height: 90vh;
+  overflow-y: hidden;
+`;
+
+const Home = styled.img`
+  display: flex;
+  flex-direction: column;
+  height: 500px;
+`;
+
+const Services = styled.section`
+  display: flex;
+  flex-direction: column;
+  background-color: #d9f2e5;
+  height: 500px;
+`;
+
+const About = styled.section`
+  display: flex;
+  flex-direction: column;
+  background-color: #c6ebd8;
+  height: 500px;
+`;
+
+const Contact = styled.section`
+  display: flex;
+  flex-direction: column;
+  background-color: #b4e4cb;
+  height: 500px;
 `;
 
 export default App;
