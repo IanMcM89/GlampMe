416e1a05bd99eb83870253b6c646c135ddd36ea4
Ian <ianmcm.89@gmail.com>
2023-02-22 03:52:34 -0500

    add package details

--- client/src/components/pages/Packages.js
+++ client/src/components/pages/Packages.js
@@ -3,19 +3,39 @@ import styled from "styled-components";
 function Packages() {
   return (
     <Wrapper>
-      <h3 style={{ margin: '1%' }}>PACKAGES</h3>
-      <p style={{ margin: '1%' }}>Choose the best options for your stay</p>
+      <h3 style={{ margin: "1%" }}>PACKAGES</h3>
+      <p style={{ margin: "1%" }}>Choose the best options for your stay</p>
       <Divider />
-      <div>
-        <Image src="/images/packages/standard.jpg" alt=""/>
-        <div></div>
-      </div>
-      <div>
-        <Image src="/images/packages/couples.jpg" alt=""/>
-      </div>
-      <div>
-        <Image src="/images/packages/girls.jpg" alt=""/>
-      </div>
+      <Package>
+        <Image src="/images/packages/standard.jpg" alt="" />
+        <Details>
+          <Title>Standard</Title>
+          <Info>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod.
+          </Info>
+        </Details>
+      </Package>
+      <Package>
+        <Image src="/images/packages/couples.jpg" alt="" />
+        <Details>
+          <Title>Couples Package</Title>
+          <Info>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod.
+          </Info>
+        </Details>
+      </Package>
+      <Package>
+        <Image src="/images/packages/girls.jpg" alt="" />
+        <Details>
+          <Title>Girls' Night</Title>
+          <Info>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod.
+          </Info>
+        </Details>
+      </Package>
     </Wrapper>
   );
 }
@@ -42,6 +62,33 @@ const Image = styled.img`
   border: solid 1px lightgray;
   width: 90%;
   margin 5%;
+  margin-bottom: 25%;
+`;
+
+const Package = styled.div`
+  position: relative;
+`;
+
+const Details = styled.div`
+  position: absolute;
+  background-color: white;
+  text-align: center;
+  width: 80%;
+  bottom: 0;
+  right: 10%;
+`;
+
+const Title = styled.header`
+  background-color: #333333;
+  color: white;
+  margin-bottom: 0;
+  padding 2%;
+`;
+
+const Info = styled.p`
+  border: 1px solid darkgray;
+  padding 4%;
+  margin-top: 0;
 `;
 
 export default Packages;
